e4814a3510a5709423b5be6828d49cd10228f321
Scott Riley <scott.riley111@gmail.com>
2016-04-06 11:38:19 +0100

    qs() working

--- specs/tests.js
+++ specs/tests.js
@@ -2,10 +2,15 @@ describe("qs() should", function () {
     var el = document.createElement("div");
     el.id = 'myDiv';
     document.body.appendChild(el);
-    var myEl = document.getElementById('myDiv');
+    var el = document.createElement("div");
+    el.class = 'myDivClass';
+    document.body.appendChild(el)
 
     it("return a single element", function () {
-
+      var myEl = document.getElementById('myDiv');
+      var result = qs('#myDiv');
+      assert.equal(result, myEl);
     });
- 
+
+
 });
